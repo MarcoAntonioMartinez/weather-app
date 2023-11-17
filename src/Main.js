40c09e7ff92ca7727f536e9e443c6a99a51cbc16
Marco Martinez <marco.antoni.martinez@gmail.com>
2023-11-16 21:52:58 -0600

    merged changes from first steps

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+function Main() {
+    return (
+      <div className="Main">
+        //label for this input
+        //input for selecting a city/zip code/whatever
+        
+        //label for this weather display
+        //container to display weather
+
+
+        //not sure maybe also this is where api stuff goes to actually 
+        //get the weather
+      </div>
+    );
+  }
+  
+  export default App;
