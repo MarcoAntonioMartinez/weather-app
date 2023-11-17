a408797e17dea9f89c877bb0edf806f3709d107b
Marco Martinez <marco.antoni.martinez@gmail.com>
2023-11-17 12:14:40 -0600

    Removed some lines and a file

---
+++ src/Main.js
@@ -0,0 +1,26 @@
+function Main() {
+  return (
+    <body>
+      <div className="Main">
+        //label for this input
+      
+      <div className="city">
+        <label for="city">City:</label>
+        <input type="text" name="city"/>
+      </div>
+      //label for this weather display
+      //container to display weather
+      <div className="weather">
+       //this is where all the weather api stuff gets displayed
+       
+      </div>
+
+    </div>
+   
+   
+  </body>
+  
+  );
+}
+
+export default App;
