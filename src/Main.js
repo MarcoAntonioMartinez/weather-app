0712487ca1a0c86ce873f4f462ba456311790779
Marco Martinez <marco.antoni.martinez@gmail.com>
2024-01-19 18:31:39 -0600

    Made app ready to display cities dropdown box

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,25 @@
-//import Cities from './Cities.js'
+import Cities from './Cities.js'
 
 function Main() {
   return (
     <body>
       <div className="Main">
+        {/* placeholder: label for this input*/}
+      
+      <div className="city">
+        <label for="city">City:</label>
         
-      
-        <div className="city">
-          <label for="city">City:</label>
-          <input type="text" name="city"/>
-      
+       <Cities />
 
-          
-        </div>
-     
-        <div className="weather">
         
-        
-        </div>
+      </div>
+      {/* placeholder : label for this weather display
+        container to display weather*/}
+      <div className="weather">
+       {/* placeholder: this is where all the weather api stuff gets displayed */}
+       
+       
+      </div>
 
     </div>
    
